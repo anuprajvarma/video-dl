973be99b4e1c7e30ff57bf22658e7ac6db8bd8e8
anuprajvarma <anupraj1854@gmail.com>
2022-07-18 10:40:30 +0530

    code reafactor

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Particle from './particle'
 import Footer from './footer'
 import './nav.css'
 
 const Home = () => {
+    const [VideoUrl, setUrl] = useState('');
+    const [id, setId] = useState('');
+
+    const URL = (e) => {
+        setUrl(e.target.value);
+        console.log(VideoUrl)
+    }
+
+
     return (
         <div className='home-div'>
             <div className='Box'>
-                <h1>asldfsdfho</h1>
+                <div>
+                    <div className='search-box'>
+                        <form onSubmit={(e) => e.preventDefault()}>
+                            <input className='input-box' type='text' onChange={URL} placeholder='Paste Youtube Video Url'></input>
+                        </form>
+                    </div>
+                </div>
+
             </div>
             <Particle className='particle' />
             <Footer />
